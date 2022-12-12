19061ffc2b4ac518e3393de094d7d2f5a135c1d4
MaksChojniak <73464378+MaksChojniak@users.noreply.github.com>
2022-12-12 06:08:00 +0100

    dodanie czesci systemu przeciwnikow

--- Dangeon.py
+++ Dangeon.py
@@ -1,9 +1,20 @@
-﻿import random, msvcrt, os
+import random, msvcrt, os
+
+
+punkty = 0
+poziom = 0
+punkty_zycia = 100
+walka = False
 
 
 # wypisz legende
-def legend(poziom):
-    text = "-----Legenda-----          -----Poruszanie sie-----          poziom -> " + str(poziom) + "\n\n" + "   gracz -> A                 gora -> w\n" + "   przeciwnik -> ☺            dol -> s\n" +  "   sciana -> ░                lewo -> a\n" + "   przejscie dalej -> O       prawo -> d\n"
+def legend():
+
+    global punkty
+    global poziom
+    global punkty_zycia
+
+    text = "-----Legenda-----      -----Poruszanie sie-----          -----Statystyki-----" + "\n\n" + "   gracz -> A                 gora -> w                      poziom -> " + str(poziom) + "\n" + "   przeciwnik -> ☺            dol -> s                       punkty -> " + str(punkty) + " \n" +  "   sciana -> ░                lewo -> a                      zycie -> " + str(punkty_zycia) + " hp\n" + "   przejscie dalej -> O       prawo -> d\n"
     return text
 
 
@@ -14,11 +25,13 @@ def legend(poziom):
 def spawn_objects_in_map(string1, value, string2):
     string_value = ""
     if value == 8:
-        string_value = "A"  #☺
+        string_value = "A"
     elif value == 1:
         string_value = "░"
     elif value == 5:
         string_value = "O"
+    elif value == 7:
+        string_value = "☺"
     else:
         string_value = " "
 
@@ -53,7 +66,7 @@ def generate_grid(tab):
 
 
 # generuje liste z wartosciami odopwiednimi dla danego przedmiotu
-def generate_map_list(rozmiar, iloszScian):
+def generate_map_list(rozmiar, iloszScian, iloscPrzeciwnikow):
     map_table = []
 
     for i in range(rozmiar[0]):
@@ -71,6 +84,13 @@ def generate_map_list(rozmiar, iloszScian):
         wall_pos = [ random.randint( 1, rozmiar[0] - 2 ), random.randint( 0, rozmiar[1] - 1 ) ]
         map_table[ wall_pos[0] ] [ wall_pos[1] ] = 1
 
+    for i in range(iloscPrzeciwnikow):
+        x = random.randint( 0, rozmiar[1] - 1 )
+        y = random.randint( 1, rozmiar[0] - 2 )
+        if map_table[y][x] != 1:
+            enemy_pos = [ y, x ]
+            map_table[ enemy_pos[0] ] [ enemy_pos[1] ] = 7
+    
     # generuj pozycje portalu
     portal_pos = [ rozmiar[0] - 1,  random.randint( 0, rozmiar[1] - 1 ) ]
     map_table[ portal_pos[0] ] [ portal_pos[1] ] = 5
@@ -83,11 +103,13 @@ def generate_map_list(rozmiar, iloszScian):
 
 
 # generuj mape zaleznie od poziomu
-def generate_map(aktualny_poziom):
+def generate_map():
+    global poziom
+
     map_list = []
 
-    if aktualny_poziom < 5:
-        map_list = generate_map_list([10,10], 15)
+    if poziom < 5:
+        map_list = generate_map_list([10,10], 25, 15)
 
     return map_list
 
@@ -143,7 +165,12 @@ def movement_system():
 
 
     
-def player_movement(map_table, poziom):
+def player_movement(map_table):
+
+    global punkty
+    global poziom
+    global walka
+
     xz_axis = movement_system()
     player_pos = []
     before_player_pos = []
@@ -161,18 +188,46 @@ def player_movement(map_table, poziom):
         player_pos[1] += xz_axis[0]
 
     if map_table[player_pos[0]][player_pos[1]] == 5:
-        poziom += 1
+        poziom = poziom + 1
 
-    if map_table[player_pos[0]][player_pos[1]] != 1:
+    if map_table[player_pos[0]][player_pos[1]] != 1 and map_table[player_pos[0]][player_pos[1]] != 7:
         map_table[before_player_pos[0]][before_player_pos[1]] = 0
         map_table[player_pos[0]][player_pos[1]] = 8
 
-    
+    if map_table[player_pos[0]][player_pos[1]] == 7:
+        player_pos = before_player_pos
+        walka = True
+
+
+    punkty = punkty + 1
+
     os.system('cls')
-    print(legend(poziom))
+    print(legend())
     print(generate_grid(map_table))
     return poziom
 
+
+
+def display_fight_scene():
+    global walka
+
+    print("To Jest Walk z Trolem \n Elo Jesetm Zenek Nie Zabijesz mnie ahhaha \n gess  \n gergeg \n regergeg")
+
+    xz_axis = movement_system()
+
+
+
+def fight_with_enemy(map_table):
+    global walka
+
+    os.system('cls')
+    display_fight_scene()
+
+    walka = False
+
+    os.system('cls')
+    print(legend())
+    print(generate_grid(map_table))
 
 
 
@@ -180,17 +235,22 @@ def player_movement(map_table, poziom):
 
 
 def update():
-    poziom = 0
     map_table = []
 
-    print(legend(poziom))
-    map_table = generate_map(poziom)
+    global poziom
+    global walka
+
+    print(legend())
+    map_table = generate_map()
     while True:
-        if poziom != player_movement(map_table, poziom):
-            poziom += 1
+        if walka == True:
+            #os.system('cls')
+            fight_with_enemy(map_table)
+
+        if poziom != player_movement(map_table):
             os.system('cls')
-            print(legend(poziom))
-            map_table = generate_map(poziom)
+            print(legend())
+            map_table = generate_map()
 
 
 
